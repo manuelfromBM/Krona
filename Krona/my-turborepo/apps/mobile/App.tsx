6c5d72958bf435f55e9bdff7a49bb8a2c7e569c7
manuelfromBM <ariel.bm.enterprise@gmail.com>
2025-11-04 20:25:32 -0300

    add(ui): Logica de navigation para flujo de la app

--- Krona/my-turborepo/apps/mobile/App.tsx
+++ Krona/my-turborepo/apps/mobile/App.tsx
@@ -1,11 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native"
 import { createStackNavigator } from "@react-navigation/stack";
-import ProfileScreen from './src/screens/profile/Profile'
 import React from 'react';
 import TabNavigator from './src/navigation/tabNavigation/TavNavigator';
-import Publicaciones from 'src/components/Feed/Publicacion';
 
 const Stack = createStackNavigator();
 
@@ -17,11 +13,3 @@ const App: React.FC = () => {
   );
 }
 export default App;
-
-
-      // <Stack.Navigator>
-      //   {/* <Stack.Screen name="Feed" component={FeedScreen} /> */}
-      //   <Stack.Screen name="Profile" component={ProfileScreen} />
-      //   {/*Componente de login san_martin*/}
-      //   <Stack.Screen name="Feed" component={Publicaciones} />
-      // </Stack.Navigator>
---
+++ Krona/my-turborepo/apps/mobile/src/navigation/stackNavigation/ProfileStackNavigator.tsx
@@ -0,0 +1,22 @@
+// src/navigation/ProfileStackNavigator.tsx
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import ProfileScreen from "src/screens/profile/Profile";
+//import Settings from "src/components/Profile/Settings";
+//import type { ProfileStackParamList } from "src/navigation/types";
+
+type ProfileStackParamList = {
+  Profile: { userId?: string } | undefined;
+  Settings: undefined;
+};
+
+const Stack = createNativeStackNavigator<ProfileStackParamList>();
+
+export default function ProfileStackNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Profile" component={ProfileScreen} options={{ title: "Perfil" }} />
+      {/* <Stack.Screen name="Settings" component={Settings} options={{ title: "Ajustes" }} /> */}
+    </Stack.Navigator>
+  );
+}
