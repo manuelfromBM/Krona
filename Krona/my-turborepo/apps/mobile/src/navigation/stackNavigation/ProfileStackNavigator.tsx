a89a5911f4c8ec86fc66893751530f531f60713d
bastian <bastianmadrid72@gmail.com>
2026-01-18 21:23:19 -0300

    fix(admin): solucionando conflictos en stack

--- Krona/my-turborepo/apps/mobile/src/navigation/stackNavigation/ProfileStackNavigator.tsx
+++ Krona/my-turborepo/apps/mobile/src/navigation/stackNavigation/ProfileStackNavigator.tsx
@@ -2,14 +2,11 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ProfileScreen from "src/screens/profile/Profile";
 import EditProfileScreen from "src/screens/editProfile/EditProfile";
-<<<<<<< HEAD
-=======
 import AdministradorScreen from "src/screens/administrador/Administrador";
 import ServiciosScreenCrud from "src/screens/administrador/Crud/Crud";
 import ScreenAgenda from "src/screens/agenda/Agenda";
 //import Settings from "src/components/Profile/Settings";
 //import type { ProfileStackParamList } from "src/navigation/types";
->>>>>>> ca1f2b5066781ea51c3284b7ff24ae2f3f496a80
 
 type ProfileStackParamList = {
   Profile: { userId?: string } | undefined;
@@ -27,11 +24,8 @@ export default function ProfileStackNavigator() {
       <Stack.Screen name="Profile" component={ProfileScreen} options={{ title: "Perfil" }} />
       <Stack.Screen name="Agenda" component={ScreenAgenda} options={{ title: "Agenda" }}></Stack.Screen>
       <Stack.Screen name="EditarPerfil" component={EditProfileScreen} options={{ title: "EditarPerfil" }} />
-<<<<<<< HEAD
-=======
       <Stack.Screen name="AdministradorScreen" component={AdministradorScreen} options={{ title: "Panel Admin" }} />
       <Stack.Screen name="ServiciosCrud" component={ServiciosScreenCrud} options={{ title: "Servicios (CRUD)" }} />
->>>>>>> ca1f2b5066781ea51c3284b7ff24ae2f3f496a80
     </Stack.Navigator>
   );
 }
