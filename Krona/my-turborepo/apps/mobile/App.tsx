39ed412c05e519d756407f631266c76d48c0a206
CristianSanMartinn <cr.sanmartino.react@gmail.com>
2026-01-26 22:21:37 -0300

    guardando cambios no guardados

--- Krona/my-turborepo/apps/mobile/App.tsx
+++ Krona/my-turborepo/apps/mobile/App.tsx
@@ -1,9 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native"
-import { createStackNavigator } from "@react-navigation/stack";
 import React from 'react';
 import TabNavigator from './src/navigation/tabNavigation/TavNavigator';
 
-const Stack = createStackNavigator();
 
 const App: React.FC = () => {
   return (
